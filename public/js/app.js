335ba6d59bff977b96aa34a7da9631b03e59593f
John <john.grindall@gmail.com>
2018-05-19 20:43:20 +0100

    refactor

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,7 @@
 var canvas = document.getElementById("gameCanvas"), gl = canvas.getContext("webgl2");
 
-var points = {
-	a : [1, 1, 1],
-	b : [1, -1, 1],
-	c : [1, 1, -1],
-	d : [1, -1, -1],
-	e : [-1, 1, 1],
-	f : [-1, -1, 1],
-	g : [-1, 1, -1],
-	h : [-1, -1, -1],
-};
-
-var v = [points.g, points.e, points.c,      points.c, points.e, points.a,      points.c, points.a, points.d,      points.d, points.a, points.b,    points.g, points.h, points.d,      points.g, points.d, points.c];
+var v = [POINTS.g, POINTS.e, POINTS.c,             POINTS.c, POINTS.e, POINTS.a,           POINTS.c, POINTS.a, POINTS.d,                     POINTS.d, POINTS.a, POINTS.b,               POINTS.g, POINTS.h, POINTS.d,                POINTS.g, POINTS.d, POINTS.c];
 var c = [COLORS.RED, COLORS.RED, COLORS.RED,       COLORS.RED, COLORS.RED, COLORS.RED,       COLORS.GREEN, COLORS.GREEN, COLORS.GREEN,       COLORS.GREEN, COLORS.GREEN, COLORS.GREEN,    COLORS.BLUE, COLORS.BLUE, COLORS.BLUE,     COLORS.BLUE, COLORS.BLUE, COLORS.BLUE];
-
-v = _.flatten(v);
-c = _.flatten(c);
 
 var _compileShader = function(id, type){
     var shader = gl.createShader(type);
@@ -41,7 +27,7 @@ var _makeProgram = function(ids, types){
 	return prog;
 };
 
-_setupBuffer = function(prog, attribName, buffer, length){
+var _setupBuffer = function(prog, attribName, buffer, length){
 	var attribLoc = gl.getAttribLocation(prog, attribName);
 	gl.enableVertexAttribArray(attribLoc);
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
@@ -52,45 +38,57 @@ var _setUniform = function(prog, name, mat){
 	gl.uniformMatrix4fv(gl.getUniformLocation(prog, name), false, mat);
 };
 
-var vbuf = _getBuffer(v), cbuf = _getBuffer(c);
+var Obj = function(v, c, dx, speed){
+	this.v = _.flatten(v);
+	this.c = _.flatten(c);
+	this.speed = speed;
+	this.dx = dx;
+	this.setup();
+};
 
-var prog = _makeProgram(["vshader", "fshader"], [gl.VERTEX_SHADER, gl.FRAGMENT_SHADER]);
+Obj.prototype.setup = function(){
+	this.angle = 0;
+	this.modelMat = mat4.create();
+	this.vbuf = _getBuffer(this.v);
+	this.cbuf = _getBuffer(this.c);
+	this.prog = _makeProgram(["vshader", "fshader"], [gl.VERTEX_SHADER, gl.FRAGMENT_SHADER]);
+	this.vao = gl.createVertexArray();
+	gl.bindVertexArray(this.vao);
+	_setupBuffer(this.prog, "position", this.vbuf, 3);
+	_setupBuffer(this.prog, "color", this.cbuf, 4);
+	_setUniform(this.prog, "projMat", mat4.perspective(mat4.create(), 45*Math.PI/180, 800/600, 0.1, 20));
+	_setUniform(this.prog, "viewMat", mat4.create());
+};
 
-var vao = gl.createVertexArray();
-gl.bindVertexArray(vao);
+Obj.prototype.beforeRender = function(){
+	mat4.identity(this.modelMat);
+    mat4.translate(this.modelMat, this.modelMat, [this.dx, 0, -10]);
+    mat4.rotateY(this.modelMat, this.modelMat, this.angle);
+    mat4.rotateZ(this.modelMat, this.modelMat, this.angle/5);
+    // A -> ATRR  (rotate applied first)
+    // Av -> ATRRv  (rotate applied first)
+    _setUniform(this.prog, "modelMat", this.modelMat);
+    this.angle += this.speed;
+};
 
-_setupBuffer(prog, "position", vbuf, 3);
-_setupBuffer(prog, "color", cbuf, 4);
+var cube0 = new Obj(v, c, 0, 0.025);
+var cube1 = new Obj(v, c, 3, 0.01);
+var cube2 = new Obj(v, c, -3, -0.075);
 
-var modelMat = mat4.create();
-var viewMat = mat4.create();
-var projMat = mat4.perspective(mat4.create(), 45*Math.PI/180, 800/600, 0.1, 20);
-var angle = 0;
-
-_setUniform(prog, "projMat", projMat);
-_setUniform(prog, "viewMat", viewMat);
-
+var _draw = function(obj){
+	gl.useProgram(obj.prog);
+	obj.beforeRender();
+    gl.bindVertexArray(obj.vao);
+    gl.drawArrays(gl.TRIANGLES, 0, obj.v.length/3);
+};
 
 var _render = function(){
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.enable(gl.DEPTH_TEST);
     gl.clearColor(0.65, 0.65, 0.7, 1);
-
-    mat4.identity(modelMat);
-    mat4.translate(modelMat, modelMat, [0, 0, -10]);
-    mat4.rotateY(modelMat, modelMat, angle);
-    mat4.rotateZ(modelMat, modelMat, angle/5);
-
-    // A -> ATRR  (rotate applied first)
-    // Av -> ATRRv  (rotate applied first)
-
-    angle += 0.025;
-
-    _setUniform(prog, "modelMat", modelMat);
-
-    gl.useProgram(prog);
-    gl.bindVertexArray(vao);
-    gl.drawArrays(gl.TRIANGLES, 0, v.length/3);
+    _draw(cube0);
+    _draw(cube1);
+    _draw(cube2);
     requestAnimationFrame(_render);
 };
 
